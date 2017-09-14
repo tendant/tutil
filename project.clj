cb0e087b8bf264aaa9139470f4ca6611332b250d
Lei Wang <tendant@gmail.com>
2017-09-14 13:34:43 -0700

    update 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tendant/tutil "0.1.1"
+(defproject tendant/tutil "0.1.2-SNAPSHOT"
   :description "Some Clojure util functions"
   :url "https://github.com/tendant/tutil"
   :license {:name "Eclipse Public License"
