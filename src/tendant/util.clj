cd638fda68630633eec54018305d06fe3df25347
Lei Wang <tendant@gmail.com>
2018-06-07 15:33:33 -0700

    add postwalk-execute function

--- src/tendant/util.clj
+++ src/tendant/util.clj
@@ -8,3 +8,17 @@
       true
       false)
     false))
+
+(defn postwalk-execute
+  "Recursively transforms form by replacing keys with the result of fn,
+  in which fn is original value function, fn should take 2
+  parameters [k context].  Like clojure/replace but works on any data
+  structure.  Does replacement at the leaves of the tree first."
+  [form context]
+  (walk/postwalk (fn [x]
+                   (if (and (vector? x)
+                            (= 2 (count x))
+                            (fn? (second x)))
+                     [(first x) ((second x) (first x) context)]
+                     x))
+                 form))
